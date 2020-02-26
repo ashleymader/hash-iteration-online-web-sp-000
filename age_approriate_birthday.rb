071aabc39587aeb732866bdc000672b93f2ed92f
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 13:06:01 +0000

    Automatically backed up by Learn

--- age_approriate_birthday.rb
+++ age_approriate_birthday.rb
@@ -1,5 +1,5 @@
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do 
-
+  birthday_kids.each do |name, age|
+  
 puts "Happy Birthday #{name}! You are now #{age} years old! You are too old for this."
 end
